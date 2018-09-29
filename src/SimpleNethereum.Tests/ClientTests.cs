4f53e0a2797e4f393eb67dd28699d532cf8fd2e9
ajlopez <webmaster@ajlopez.com>
2018-09-29 18:36:44 -0300

    Initial code and tests

---
+++ src/SimpleNethereum.Tests/ClientTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleNethereum.Tests
+{
+    [TestClass]
+    public class ClientTests
+    {
+        [TestMethod]
+        public void CreateCLientWithDefaultConnection()
+        {
+            var client = new Client();
+
+            Assert.IsNotNull(client.Web3);
+        }
+
+        [TestMethod]
+        public async Task GetAccounts()
+        {
+            var client = new Client();
+
+            var accounts = await client.GetAccounts();
+
+            Assert.IsNotNull(accounts);
+            Assert.AreEqual(10, accounts.Length);
+        }
+    }
+}
---
+++ src/SimpleNethereum/Client.cs
@@ -0,0 +1,32 @@
+﻿using Nethereum.Web3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleNethereum
+{
+    public class Client
+    {
+        private Web3 web3;
+
+        public Client()
+        {
+            this.web3 = new Web3("http://localhost:8545");
+        }
+
+        public Web3 Web3
+        {
+            get
+            {
+                return this.web3;
+            }
+        }
+
+        public async Task<String[]> GetAccounts()
+        {
+            return await web3.Eth.Accounts.SendRequestAsync();
+        }
+    }
+}
