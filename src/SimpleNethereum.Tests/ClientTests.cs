7e611069cc243b042da5ae7df7f3866ee5d998f9
ajlopez <webmaster@ajlopez.com>
2018-10-01 16:33:21 -0300

    Transfer

--- src/SimpleNethereum.Tests/ClientTests.cs
+++ src/SimpleNethereum.Tests/ClientTests.cs
@@ -40,5 +40,24 @@ namespace SimpleNethereum.Tests
             Assert.IsNotNull(balance);
             Assert.IsFalse(balance.Value.Equals(BigInteger.Zero));
         }
+
+        [TestMethod]
+        public async Task Transfer()
+        {
+            var client = new Client();
+
+            var accounts = await client.GetAccounts();
+
+            var balance0 = (await client.GetBalance(accounts[0])).Value;
+            var balance1 = (await client.GetBalance(accounts[1])).Value;
+
+            var hash = await client.Transfer(accounts[0], accounts[1], new HexBigInteger(new BigInteger(1000)), new HexBigInteger(new BigInteger(21000)), new HexBigInteger("0x00"));
+
+            var newbalance0 = (await client.GetBalance(accounts[0])).Value;
+            var newbalance1 = (await client.GetBalance(accounts[1])).Value;
+
+            Assert.AreEqual(BigInteger.Subtract(balance0, new BigInteger(1000)), newbalance0);
+            Assert.AreEqual(BigInteger.Add(balance1, new BigInteger(1000)), newbalance1);
+        }
     }
 }
--- src/SimpleNethereum/Client.cs
+++ src/SimpleNethereum/Client.cs
@@ -1,4 +1,5 @@
 ﻿using Nethereum.Hex.HexTypes;
+using Nethereum.RPC.Eth.DTOs;
 using Nethereum.Web3;
 using System;
 using System.Collections.Generic;
@@ -33,6 +34,12 @@ namespace SimpleNethereum
         public async Task<HexBigInteger> GetBalance(string account)
         {
             return await web3.Eth.GetBalance.SendRequestAsync(account);
+        }
+
+        public async Task<string> Transfer(string fromAccount, string toAccount, HexBigInteger value, HexBigInteger gas, HexBigInteger gasPrice)
+        {
+            TransactionInput ti = new TransactionInput(null, toAccount, fromAccount, gas, gasPrice, value); 
+            return await web3.Eth.Transactions.SendTransaction.SendRequestAsync(ti);
         }
     }
 }
